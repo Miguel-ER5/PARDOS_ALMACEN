fabfad7282fa9a35cc8d013ad1fe58e0da7f3900
USER <USER@USER-PC>
2016-07-12 20:29:44 -0500

    commit

--- php/login.php
+++ php/login.php
@@ -5,15 +5,16 @@ require_once('conexionBD.php');
 conectar();
 
 function  verificar_login($user,$password)   {
-    $sql = "SELECT dni,contrasena,nombre FROM usuario where dni='$user' AND contrasena='$password'";
+    $sql = "SELECT dni,contrasena,nombre,area FROM usuario where dni='$user' AND contrasena='$password'";
     $rec = mysql_query($sql);
     $fila = mysql_fetch_row($rec);
     $pass = $fila[1];
     $us = $fila[0];
     $nom=$fila[2];
+    $area=$fila[3];
     $_SESSION['userid'] = $nom ;
     if($pass == $password AND $us ==$user)
-    {return 1;}
+    {return $area;}
     else
     {return 0;}
 }
@@ -21,10 +22,8 @@ function  verificar_login($user,$password)   {
 
 if(!isset($_SESSION['userid']))
 {
-       if(verificar_login($_POST['usuario'],$_POST['contraseña']) == 1)
+       if(verificar_login($_POST['usuario'],$_POST['contraseña']) == 'Administracion')
         {
-        
-           
            $_SESSION['logged']='yes'; 
            
             header("location:../indexIntranet.php");
